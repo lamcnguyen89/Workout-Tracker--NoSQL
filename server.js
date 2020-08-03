ac64bc0af16fd8833a1a281afc12a41e56949a7a
Lam Nguyen <lamcnguyen89@gmail.com>
2020-08-02 20:32:14 -0400

    added the server.js part of the application

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
-// Import npm packages.
+// Import npm packages and dependencies
+const express = require("express");
+const mongoose = require("mongoose");
 
-// Use express Middleware ( urlEncoded, json and static ).
 
 // Create PORT and assign to a variable.
+const PORT = process.env.PORT || 3000;
+
+// const db = require("./models");
+const app = express();
+
+// Use express Middleware ( urlEncoded, json and static ).
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
 
 // Connect to Mongo Db.
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // Import your routes (API and HTML Routes).
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 // create code to start the server and listen to requests.
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+
+
