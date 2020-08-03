44b3ea3529fe9a80beab3d92e1bf9c31504377ed
Lam Nguyen <lamcnguyen89@gmail.com>
2020-08-02 21:12:37 -0400

    Added the Workout.JS to create the database that stores the workouts

--- model/Workout.js
+++ model/Workout.js
@@ -1,10 +1,6 @@
 // Application uses MongoDB for its database
 // Include the following properties to the Workout Model:
-
-
     // day (type -> Date)
-
-
     // exercises (type -> Array)
         // Inside the "exercises" property Array, you will store each of the exercises for that workout as objects.
         // Each object inside the "exercises" property will have the following properties:
@@ -15,3 +11,43 @@
             // - reps (type -> Number)
             // - sets (type -> Number)
             // - distance (type -> Number)
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String
+        },
+        name: {
+            type: String
+        },
+        duration: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }]
+
+});
+
+const Workouts = mongoose.model("Workouts", workoutSchema);
+
+module.exports = Workouts;
